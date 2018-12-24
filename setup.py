aa3317c50cdf41646c8aa7daeefcbd47b3f0cbb8
kobiburkis <kobi.burkis@yahoo.com>
2018-12-24 20:57:57 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Lending Club analysis - credit risk',
+    author='K&N',
+    license='BSD-3',
+)
